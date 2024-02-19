7660bf3ef91935cc9e6bd8b4b94c59dae0deded8
jillo <jill.l.oreilly@gmail.com>
2024-02-19 23:47:50 +0000

    Update copy on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,16 @@ export default function Home() {
       <div className="container mt-5">
         
         <main>
-          <section id="about" class="container">
-              <div class="p-3 col-sm-12 col-md-12 col-lg-12 d-flex text-center align-items-center justify-content-center flex-column flex-md-column flex-lg-row">
+          <section id="about" className="container">
+              <div className="p-3 col-sm-12 col-md-12 col-lg-12 d-flex text-center align-items-center justify-content-center flex-column flex-md-column flex-lg-row">
                   <img src="./src/pages/images/jill-avatar-sm.png"
-                      class="m-4 ms-3 float-md-start float-end rounded-top-circle img-fluid"
+                      className="m-4 ms-3 float-md-start float-end rounded-top-circle img-fluid"
                       alt="Avatar image of Jill O'Reilly" />
                   <div>
                     <h1>Hi! I'm Jill ...</h1>
-                    <p>and welcome to my portfolio website. I'm a front-end developer based in London but originally from Australia. I have had a career break after having my 2 daughters and I'm keen to get back into development. I love HTML, CSS and design and I am excited to learn new technologies like React,node, git and more!</p>
-                    <p>Outside of web development I love scuba diving and travelling as well as fashion, reading and fibre art such as cross stitch.</p>
-                    <p>Have a look at the showcase below of some of my work from the web development bootcamp as well as projects from my work as a developer.</p>
+                    <p>Welcome to my portfolio website. I'm a front-end developer based in London but originally from Australia. Following a career break to focus on raising my two daughters, I'm now eager to dive back into the world of development.</p>
+                    <p>My passion lies in crafting robust and scalable frontend applications that prioritize exceptional user experiences.</p>
+                    <p>Feel free to explore my <a href="">project showcase</a> to get a glimpse of some of my work.</p>
                   </div>
               </div>
           </section>
