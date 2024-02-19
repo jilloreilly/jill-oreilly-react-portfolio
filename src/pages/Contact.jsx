cb23f1e82f66f7cbe8ee98123d3f3f05ad888a4e
jillo <jill.l.oreilly@gmail.com>
2024-02-19 23:47:19 +0000

    Initial contact page - needs updating and styling

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,31 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 const Contact = () => {
 
   return (
     <>
-      <div>
-        <h1>Contact</h1>
+      <div className="contact container mt-5">
+        <h1>Get in touch</h1>
+        <div className="d-flex justify-content-center">
+        <Form className='p-4'>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        </div> 
       </div>
     </>
   )
