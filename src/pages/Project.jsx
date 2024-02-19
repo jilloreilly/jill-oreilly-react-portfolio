1d3d91e3f8aee8083036dca3616c556114f68a8d
jillo <jill.l.oreilly@gmail.com>
2024-02-19 22:31:11 +0000

    Add useParams to grab ID to link to corect project page. Simplify function and remove .map

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,38 +1,33 @@
 import projects from '../data/projects.json'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-// import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+
 
 export default function Project(props) {
-  // let params = useParams()
+  let {id} = useParams()
   
-  let projectInfo = projects.filter((project) => (
-    project.id === props.id ? project : null
-  ))
+  let projectInfo = projects.filter((project) => project.id == id )
+  console.log(projectInfo)
+
+  let {title, image, description, github, deployed} = projectInfo[0];
     
-  let listProject = projectInfo.map((item, i) => {
-
-    let {title, image, description, github, deployed} = item;
-
-      return (
-        <div key={i}>
+  return (
+      <div>
+        <div key={id}>
           <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={item.image} />
+            <Card.Img variant="top" src={image} />
             <Card.Body>
-              <Card.Title>{item.title}</Card.Title>
-              <Card.Text>{item.description}</Card.Text>
+              <Card.Title>{title}</Card.Title>
+              <Card.Text>{description}</Card.Text>
               {/* <Button variant="primary" href={item.github}>Go somehwere</Button>
               <Button variant="primary" href={item.github}>Go somehwere</Button> */}
-              <Card.Link href={item.github}>View the GitHub repo</Card.Link>
-              <Card.Link href={item.deployed}>View the deployed site</Card.Link>
+              <Card.Link href={github}>View the GitHub repo</Card.Link>
+              <Card.Link href={deployed}>View the deployed site</Card.Link>
             </Card.Body>
           </Card>
         </div>
-      )
-    }) 
-
-    return (
-      <div>{listProject}</div>
+      </div>
     )
 
 }
