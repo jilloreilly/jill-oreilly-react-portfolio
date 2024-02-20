d2b11eef4ccbc99021df99af70555cfbe95e5abd
jillo <jill.l.oreilly@gmail.com>
2024-02-20 17:04:00 +0000

    Add footer links to email/github/linkedin,CV and associated styling and icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,18 @@
 export default function Footer() {
-  return (
-    <>
-      <footer className="text-center py-3">
-        <ul className="nav ml-auto justify-content-center">
-            <li className="nav-item">
-                <a className="nav-link" href="#about">About me</a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="#work">Work</a>
-            </li>
-
-            <li className="nav-item">
-                <a className="nav-link" href="#skills">Skills</a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="#contact">Contact me</a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="./assets/Jill-OReilly-CV-Sep22.pdf" target="_blank">Resume</a>
-            </li>
-        </ul>
-        <p>© 2024 Designed and developed by Jill O'Reilly</p>
-    </footer>
-    </>
-  )
+    return (
+        <>
+            <footer className="text-center py-3">
+                {/* Contact details */}
+                <section id="contact" className="container p-3">
+                   <div id="contact-info" className="d-flex flex-row justify-content-evenly">
+                      <p><a href="mailto:jill.l.oreilly@gmail.com"><img src="/images/icons/icons8-email-50.png" alt="Email me" /></a></p>
+                       <p><a href="https://github.com/jilloreilly" target="_blank"><img src="/images/icons/icons8-github-50.png" alt="Github" /></a></p>
+                       <p><a href="https://www.linkedin.com/in/jill-o-reilly/" target="_blank"><img src="/images/icons/icons8-linked-in-50.png" alt="Linked In" /></a></p>
+                       <p><a href="/assets/Jill-OReilly-CV.pdf" target="_blank"><img src="/images/icons/icons8-resume-50.png" alt="My Resume" /></a></p>
+                  </div>
+                </section>
+                <p>© 2024 Designed and developed by Jill O'Reilly</p>
+            </footer>
+        </>
+    )
 }
