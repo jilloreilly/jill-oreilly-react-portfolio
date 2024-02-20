d8e9466ca463ef404ab2466afd72ce124c91ce8d
jillo <jill.l.oreilly@gmail.com>
2024-02-20 16:49:30 +0000

    add fixed height to project gallery img. remove reference to large image. Add styling object-fit:cover so that gallery image fills the container

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -10,21 +10,23 @@ export default function Project(props) {
   let projectInfo = projects.filter((project) => project.id == id )
   console.log(projectInfo)
 
-  let {title, image, imageLg, description, github, deployed} = projectInfo[0];
+  let {title, image, description, github, deployed} = projectInfo[0];
     
   return (
     <div className='container project-detail mt-5' key={id}>
       <h1 className='mb-5'>- Project detail -</h1>
-      <Card>
+      <div className='d-flex justify-content-center'>
+      <Card className='d-flex justify-content-center'>
       <Card.Title>{title}</Card.Title>
-        <Card.Img variant="top" src={imageLg} alt={title} />
+        <Card.Img variant="top" src={image} alt={title} />
         <Card.Body>
           <Card.Text>{description}</Card.Text>
-          <Card.Link href={github}>View the GitHub repo</Card.Link>
+          <Card.Link href={github} target='_blank'>View the GitHub repo</Card.Link>
           <span>  |  </span>
-          <Card.Link href={deployed}>View the deployed site</Card.Link>
+          <Card.Link href={deployed} target='_blank'>View the deployed site</Card.Link>
         </Card.Body>
       </Card>
+      </div>
     </div>
   )
 
