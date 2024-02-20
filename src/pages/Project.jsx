68620fcee681a657a1f6c6e6143efced591dc16a
jillo <jill.l.oreilly@gmail.com>
2024-02-20 13:09:25 +0000

    Add stlying for project detail page, add new key-value in json for large project image, add alt attribute to img

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -10,16 +10,18 @@ export default function Project(props) {
   let projectInfo = projects.filter((project) => project.id == id )
   console.log(projectInfo)
 
-  let {title, image, description, github, deployed} = projectInfo[0];
+  let {title, image, imageLg, description, github, deployed} = projectInfo[0];
     
   return (
     <div className='container project-detail mt-5' key={id}>
+      <h1 className='mb-5'>- Project detail -</h1>
       <Card>
       <Card.Title>{title}</Card.Title>
-        <Card.Img variant="top" src={image} />
+        <Card.Img variant="top" src={imageLg} alt={title} />
         <Card.Body>
           <Card.Text>{description}</Card.Text>
           <Card.Link href={github}>View the GitHub repo</Card.Link>
+          <span>  |  </span>
           <Card.Link href={deployed}>View the deployed site</Card.Link>
         </Card.Body>
       </Card>
