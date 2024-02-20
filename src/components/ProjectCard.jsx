d81ecaf5051b9a006a45509fc70f9db7e24c9262
jillo <jill.l.oreilly@gmail.com>
2024-02-20 16:50:16 +0000

    Remove unneeded html

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,7 +6,7 @@ export default function ProjectCard(props) {
   return (
     <div className='col'>
       <Link to={`/project/${props.id}`}>
-        <Card className="bg-dark h-100 text-white">
+        <Card className="text-white">
           <Card.Img src={props.image} alt={props.title} />
           <Card.ImgOverlay>
             <Card.Title>{props.title}</Card.Title>
@@ -14,20 +14,6 @@ export default function ProjectCard(props) {
           </Card.ImgOverlay>
         </Card>
       </Link>
-
-
-
-    {/* <Link to={`/project/${props.id}`}>
-    <Card className='h-100'>
-      <Card.Img variant="top" src={props.image} alt={props.title} />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text>{props.description}</Card.Text>
-        <a href={props.github}><Button variant="primary">Repo</Button></a>
-        <a href={props.deployed}><Button variant="primary">Deployed site</Button></a>
-      </Card.Body>
-    </Card>
-    </Link> */}
     </div>
   );
 }
