b4d0946785b8d620d0a3af4d5165775140dd8da2
jillo <jill.l.oreilly@gmail.com>
2024-02-26 21:32:17 +0000

    Remove image2 from projects.json, not needed anymore as HashRouter has fixed it. Remove reference to image2 in Project.jsx

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -9,21 +9,25 @@ export default function Project(props) {
   let projectInfo = projects.filter((project) => project.id == id )
   console.log(projectInfo)
 
-  let {title, image, image2, description, github, deployed} = projectInfo[0];
+  let {title, image, description, github, deployed} = projectInfo[0];
     
   return (
     <div className='container project-detail mt-5' key={id}>
       <h1 className='mb-5'>- Project detail -</h1>
       <div className='d-flex justify-content-center'>
       <Card>
-        <Card.Img variant="top" src={image2} alt={title} />
+        <Card.Img variant="top" src={image} alt={title} />
         <Card.Body className='p-5'>
           <Card.Title>{title}</Card.Title>
           <Card.Text>{description}</Card.Text>
           <div className='d-flex justify-content-evenly'>
           <Card.Link href={github} target='_blank'>View the GitHub repo</Card.Link>
-          <span>  |  </span>
-          <Card.Link href={deployed} target='_blank'>View the deployed site</Card.Link>
+          {(deployed.length > 0) ? (
+            <>
+            <span>  |  </span>
+            <Card.Link href={deployed} target='_blank'>View the deployed site</Card.Link>
+            </>
+          ):('')}
           </div>
         </Card.Body>
       </Card>
